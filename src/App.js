38e32dc257666c4ef6a0fa08633bb902fedf5116
Liam Kruger <liamkruger62@gmail.com>
2022-01-04 17:46:29 -0500

    initial routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import People from './components/People';
 import AddPerson from './components/AddPerson';
+import {BrowserRouter as Router,Routes, Route, Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <People/>
-      <AddPerson/>
-    </div>
+    <Router>
+      <nav>
+        <Link to={"/"}>home</Link>
+        <Link to={"/addPerson"}>AddUser</Link>
+        <Link to={"//profile:id"}>Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/profile:id" element={<People/>} />
+        <Route path="/addPerson" element={<AddPerson/>}/>
+      </Routes>
+    </Router>
   );
 }
 
